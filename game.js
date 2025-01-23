0b4f43454887f853ca07002c16c834e70ec6525e
Blake Fogel <114951798+Blake-Fogel@users.noreply.github.com>
2025-01-23 07:18:14 -0500

    Refactor Code for new functionality
    -move code to display win,lose,tie message into it's own function
    -delete for loop
    -delete function to get human choice

--- game.js
+++ game.js
@@ -18,9 +18,12 @@ function playGame(round) {
     //number of draws
     let drawScore = 0;
     if (typeof round === 'number' && round > 0) {
-        for (let a = 0; a < round; a++) {
-            alert(playRound(getHumanChoice(),getComputerChoice()));
-        }
+
+    }
+    //#region game code stuff
+
+    //display results message
+    function displayResultsMessage() {
         if (humanScore > computerScore) {
             alert(`Congrats! You beat the computer! ${humanScore}-${computerScore}`);
         } else {
@@ -29,12 +32,8 @@ function playGame(round) {
             } else {
                 alert(`Oh no! You lost the game! ${humanScore}-${computerScore}`);
             }
-
         }
     }
-    //#region game code stuff
-
-
 
     /* 
         Get a random number between 0 inclusive and 3 exlusive
@@ -46,24 +45,6 @@ function playGame(round) {
             the "| 0" is to truncate the floating point part
         */
         return ["rock","paper","scissors"][(Math.random() * 3) | 0]
-    }
-    /* 
-        Get human choice.
-        Keep prompting until the choice is valid.
-    */
-    function getHumanChoice() {
-        /* 
-            Create an empty initial variable.
-            Enter the do while and set the variable equal to use input
-            check that user input against a regex and if it doesn't match
-            the regex will return zero and continue looping.
-            The regex is case insensitive
-        */
-        let humanChoice;
-        do {
-            humanChoice = prompt("Do you choose rock, paper, or scissors?");
-        } while (!(/^(Rock)|(Paper)|(Scissors)$/i.test(humanChoice)));
-        return humanChoice;
     }
     /* 
         this function will take in computer choice
